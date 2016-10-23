b3b7a59bbb1cdd3d2e0e682350641c3989481562
Rody Campos <rodycamposgo@gmail.com>
2016-10-23 17:42:43 -0600

    example route

--- www/services/categoryService.js
+++ www/services/categoryService.js
@@ -2,14 +2,16 @@ service.service('categoryService', function ($q, $http) {
     var service = {};
 
     service.categoryList = function () {
+        var d = $q.defer();
         $http({
             method: "GET",
             url: "http://localhost:8080/category/list"
-        }).then(function mySucces(response) {
-            console.log(response);
-        }, function myError(response) {
-            console.log(response);
+        }).then(function Succes(response) {
+            d.resolve(response.data);
+        }, function Error(response) {
+            d.reject(response.data);
         });
+        return d.promise;
     };
 
     return service;
