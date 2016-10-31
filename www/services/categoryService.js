4ee218a4b3dbbb278dc63c91bec584b7105b7458
Mike SN <Mike SN>
2016-10-30 22:19:23 -0600

    cambiando a nueva consulta

--- www/services/categoryService.js
+++ www/services/categoryService.js
@@ -13,6 +13,19 @@ service.service('categoryService', function ($q, $http) {
         });
         return d.promise;
     };
+    
+     service.categoryTenList = function () {
+        var d = $q.defer();
+        $http({
+            method: "GET",
+            url: "http://localhost:8080/category/list"
+        }).then(function Succes(response) {
+            d.resolve(response.data);
+        }, function Error(response) {
+            d.reject(response.data);
+        });
+        return d.promise;
+    };
 
     service.categoryNew = function (parameter) {
         var d = $q.defer();
--- www/view/ten/tenCtrl.js
+++ www/view/ten/tenCtrl.js
@@ -1,26 +1,45 @@
 control.controller('tenCtrl', function ($scope, categoryService,ideaService) {
     
-        var assignCategory = function (list){   
-             $scope.data=list;
-        };   
-        var assingIdeas = function(list){
-            $scope.ideas=list;
-        };
-        var error = function(object){        
-            // print     
-        };   
-        function loadCategoryList(){        
-                categoryService.categoryList()
-                        .then(assignCategory)
-                        .catch(error);
-        } ;
-        function loadIdeas(){
-            ideaService.ideaList()
-                        .then(assingIdeas)
-                        .catch(error);
-        };
-        (function init(){      
-            loadCategoryList();
-            loadIdeas();
-        })();
+       $scope.selectedItem = 1;
+    $scope.ideas = "";
+    $scope.ideasByCategory = [];
+
+    var assignCategory = function (list) {
+        $scope.categories = list;
+    };
+    var assingIdeas = function (list) {
+        $scope.ideas = list;
+        //$scope.ideasFilter();
+    };
+    var error = function (object) {
+        // print     
+    };
+    function loadCategoryList() {
+        categoryService.categoryTenList()
+                .then(assignCategory)
+                .catch(error);
+    }
+    ;
+    function loadIdeas() {
+        ideaService.ideaList()
+                .then(assingIdeas)
+                .catch(error);
+    }
+    ;
+    $scope.ideasFilter = function(categoryId) {
+        var i = 0;
+        $scope.ideasByCategory = [];
+        for (i; i < $scope.ideas.length; i++) {
+            if ($scope.ideas[i].category.id == categoryId) {
+                $scope.ideasByCategory.push($scope.ideas[i]);
+            }
+        }
+    }
+    ;
+    (function init() {
+        loadCategoryList();
+        loadIdeas();
+        //$scope.ideasFilter();
+    })();
+
 });
