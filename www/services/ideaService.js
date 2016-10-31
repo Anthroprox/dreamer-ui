538a2f2bd475f54ecc2e26888a22b07f56426c39
Mike SN <Mike SN>
2016-10-30 23:41:36 -0600

    Top ten por ideas

--- www/services/ideaService.js
+++ www/services/ideaService.js
@@ -27,6 +27,17 @@ service.service('ideaService', function ($q, $http) {
         });
         return d.promise;
     };
-
+      service.ideaTenList = function (id) {
+        var d = $q.defer();
+        $http({
+            method: "GET",
+            url: "http://localhost:8080/idea/top/"+id
+        }).then(function Succes(response) {
+            d.resolve(response.data);
+        }, function Error(response) {
+            d.reject(response.data);
+        });
+        return d.promise;
+    };
     return service;
 });
--- www/view/ten/tenCtrl.js
+++ www/view/ten/tenCtrl.js
@@ -1,3 +1,40 @@
-control.controller('tenCtrl', function ($scope, $http) {
+control.controller('tenCtrl', function ($scope, categoryService,ideaService) {
+    
+     $scope.selectedItem = 1;
+    $scope.ideas = "";
+    $scope.ideasByCategory = [];
+
+    var assignCategory = function (list) {
+        $scope.categories = list;
+    };
+    var assingIdeas = function (list) {
+        $scope.ideas = list;
+        //$scope.ideasFilter();
+    };
+    var error = function (object) {
+        // print     
+    };
+    function loadCategoryList() {
+        categoryService.categoryList()
+                .then(assignCategory)
+                .catch(error);
+    }
+    ;
+    function loadIdeas(idea) {
+        ideaService.ideaTenList(idea)
+                .then(assingIdeas)
+                .catch(error);
+    }
+    ;
+    $scope.ideasFilter = function(categoryId) {
+        loadIdeas(categoryId);
+    }
+    ;
+    (function init() {
+        loadCategoryList();
+        loadIdeas(1);
+        //$scope.ideasFilter();
+    })();
+
 
 });
